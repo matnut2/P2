4f18f6d11abefd7671e5b0b685b4ce32800a7bbd
matnut2 <matteosolda12@gmail.com>
2022-10-05 21:18:34 +0200

    secondo commit orario

--- orario/orario.cpp
+++ orario/orario.cpp
@@ -5,10 +5,37 @@ class orario{
         int sec;
 
     public:
+        orario();
+        orario(int);
+        orario(int, int);
+        orario(int, int, int);
         int Ore();
         int Minuti();
         int Secondi();
 };
+
+orario::orario(){
+        sec = 0;
+}
+
+orario::orario(int s){
+    if(s > 60 || s <= 0)
+        sec = 0;
+    else sec = s;
+}
+
+orario::orario(int o, int m){
+    if( o > 0 && o < 24 && m >= 0 && m < 60)
+        sec = o * 3600 + m * 60;
+    else sec = 0;
+}
+
+orario::orario(int o, int m, int s){
+    
+    if( o > 0 && o < 24 && m >= 0 && m < 60 && s > 0 && s < 60)
+        sec = o * 3600 + m * 60 + s;
+    else sec = 0;
+}
 
 int orario::Ore(){
     return sec / 3600;
