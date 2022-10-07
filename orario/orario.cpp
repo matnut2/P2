617b140f5141a4360f851672423ef14a74e69441
matnut2 <matteosolda12@gmail.com>
2022-10-07 15:08:16 +0200

    update orario: costruttore con parametri di default - aggiornamento

--- orario/orario.cpp
+++ orario/orario.cpp
@@ -5,13 +5,13 @@ class orario{
         int sec;
 
     public:
-        orario(int, int, int);
+        orario(int=0, int=0, int=0);
         int Ore();
         int Minuti();
         int Secondi();
 };
 
-orario::orario(int o = 0, int m=0, int s=0){
+orario::orario(int o, int m, int s){
     
     if( o >= 0 && o <= 24 && m >= 0 && m <= 60 && s >= 0 && s <= 60)
         sec = o * 3600 + m * 60 + s;
