6718ffdfbf9ac91afde50b9b78727acbc96fc051
matnut2 <matteosolda12@gmail.com>
2022-10-12 14:50:18 +0200

    Orario - Utilizzata un minimo la funzione main per testare le varie funzioni e i vari costruttori

--- orario/orario.cpp
+++ orario/orario.cpp
@@ -103,5 +103,16 @@ std::ostream & operator<<(std::ostream& os, const orario& o){
 }
 
 int main(){
+    orario ora(14, 38);
+    orario ora2(ora);
+    
+    std::cout << (ora == ora2) << std::endl;
+    orario somma(ora+ora2);
+    std::cout << (somma > ora2) << std::endl;
+    std::cout << (somma < ora2) << std::endl; 
+    std::cout << orario::OraDiPranzo() << std::endl;
+    std::cout << ora << std::endl;
+    ora.avantiDiUnOra();
+    std::cout << ora << std::endl;
 
 }
