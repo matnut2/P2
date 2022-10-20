cb840d453f2b3a4f38a54960cab51053ce4b0593
matnut2 <matteosolda12@gmail.com>
2022-10-20 11:40:16 +0200

    Orario - modifica funzioni per deep copy + aggiunte alcune funzioni per bolletta

--- orario/bolletta.cpp
+++ orario/bolletta.cpp
@@ -35,3 +35,63 @@ telefonata bolletta::Estrai_Una(){
     delete p;
     return aux;
 }
+
+bolletta& bolletta::operator=(const bolletta& b){
+    if(this != &b){
+        distruggi(first);
+        first = copia(b.first);
+    }
+    return *this;
+}
+
+bolletta::nodo* bolletta::copia(nodo* p){
+    if(!p) return nullptr;
+
+    nodo* primo = new nodo(p->info, nullptr);
+    nodo* q = primo;
+
+    while(p->next){
+        p= p->next;
+        q->next = new nodo(p->info, nullptr);
+        q = q->next;    
+    }
+
+    return primo;
+}
+
+void bolletta::distruggi(nodo* p){
+    if(p!=nullptr){
+        nodo* q = p;
+        while (p!=nullptr){
+            p = p->next;
+            delete q;
+            q = p;
+        }
+    }
+}
+
+bolletta::bolletta(const bolletta& b) : first(copia(b.first)){}
+
+orario Somma_Bolletta(bolletta b){
+    orario durata;
+    while(!b.Vuota()){
+        telefonata t = b.Estrai_Una();
+        durata = durata + t.Fine() - t.Inizio();
+    }
+
+    return durata;
+}
+
+bolletta Chiamate_A(int num, bolletta& b){
+    bolletta selezionate, resto;
+    while(!b.Vuota()){
+        telefonata t = b.Estrai_Una();
+        if(t.Numero() == num)
+            selezionate.Aggiungi_Telefonata(t);
+        else
+            resto.Aggiungi_Telefonata(t);
+    }
+
+    b = resto;
+    return selezionate;
+}
--- orario/bolletta.h
+++ orario/bolletta.h
@@ -13,13 +13,20 @@ class bolletta{
                 nodo* next;
         };
         nodo* first;
-
+        static nodo* copia(nodo*);
+        static void distruggi(nodo*);
+    
     public:
         bolletta();
+        bolletta(const bolletta&);
         bool Vuota() const;
         void Aggiungi_Telefonata(const telefonata&);
         void Togli_Telefonata(const telefonata&);
         telefonata Estrai_Una();
+        bolletta& operator=(const bolletta&);
 };
+
+orario Somma_Durata(bolletta b);
+bolletta Chiamate_A(int, bolletta&);
 
 #endif
